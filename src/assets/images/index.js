09bf5558eb32cb186aa78b5c8d1e54b40e7b7659
quangminh <minhrrgg@gmail.com>
2023-10-16 22:07:24 +0700

    build ui header 1

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+    logo: require('./logo.svg').default,
+    search: require('./search.svg').default,
+    close: require('./close.svg').default,
+}
+
+export default images
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,28 @@
 import classNames from 'classnames/bind'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import styles from './Header.module.scss'
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" />
+                    <button className={cx('clear')}>
+                        <img src={images.close} alt="close-icon" />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <img src={images.search} alt="search-icon" />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     )
 }
